a9da0f541c56852b5f8f36894432b8c4f123a234
Александр Пекшев <cjmodis@gmail.com>
2022-04-05 12:02:04 +0300

    Add Nuke build

---
+++ build/Build.cs
@@ -0,0 +1,10 @@
+using ModPlusNuke;
+using Nuke.Common.Execution;
+
+[CheckBuildProjectConfigurations]
+[UnsetVisualStudioEnvironmentVariables]
+class Build : RengaPluginBuild
+{
+    public static int Main() => Execute<Build>(x => x.Compile);
+
+}
